02eb40b5f3094efe54f81df9455449365c96d8e2
BerkayBaseski <132743890+BerkayBaseski@users.noreply.github.com>
2024-05-24 21:48:56 +0300

    git add Personel.py git commit -m "Personel.py dosyası eklendi ve değişikler yapıldı." git push origin main

---
+++ Personel.py
@@ -0,0 +1,25 @@
+class Personel:
+    def __init__(self, personel_no, ad, soyad, departman, maas):
+        self.__personel_no = personel_no
+        self.__ad = ad
+        self.__soyad = soyad
+        self.__departman = departman
+        self.__maas = maas
+
+    def get_personel_bilgileri(self):
+        return f"Personel No: {self.__personel_no}, Ad: {self.__ad}, Soyad: {self.__soyad}, Departman: {self.__departman}, Maaş: {self.__maas}"
+
+    def __str__(self):
+        return self.get_personel_bilgileri()
+
+    def get_ad(self):
+        return self.__ad
+
+    def get_soyad(self):
+        return self.__soyad
+
+    def get_departman(self):
+        return self.__departman
+
+    def get_maas(self):
+        return self.__maas
