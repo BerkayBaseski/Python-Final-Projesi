a282461a85b7bba5414ab3a5d9b4dc92791b0e46
BerkayBaseski <132743890+BerkayBaseski@users.noreply.github.com>
2024-06-09 00:42:05 +0300

    git add Hemsire.py git commit -m "Hemsire.py dosyası eklendi" git push origin main

---
+++ Hemsire.py
@@ -0,0 +1,40 @@
+from Personel import Personel
+
+class Hemsire(Personel):
+    def __init__(self, personel_no, ad, soyad, departman, maas, calisma_saati, sertifika, hastane):
+        super().__init__(personel_no, ad, soyad, departman, maas)
+        self.__calisma_saati = calisma_saati
+        self.__sertifika = sertifika
+        self.__hastane = hastane
+
+    def __str__(self):
+        return f"{super().__str__()}, Çalışma Saati: {self.__calisma_saati}, Sertifika: {self.__sertifika}, Hastane: {self.__hastane}"
+
+    def get_calisma_saati(self):
+        return self.__calisma_saati
+
+    def get_sertifika(self):
+        return self.__sertifika
+    def get_hastane(self):
+        return self.__hastane
+
+    def set_calisma_saati(self, calisma_saati):
+        self.__calisma_saati = calisma_saati
+
+    def set_sertifika(self, sertifika):
+        self.__sertifika = sertifika
+
+    def set_hastane(self, hastane):
+        self.__hastane = hastane
+
+    def maas_arttir(self):
+        yeni_maas = self.get_maas()
+        if self.__calisma_saati >= 40:
+            yeni_maas += yeni_maas * 0.20
+        elif self.__calisma_saati >= 30:
+            yeni_maas += yeni_maas * 0.15
+        elif self.__calisma_saati >= 20:
+            yeni_maas += yeni_maas * 0.10
+        self.set_maas(yeni_maas)
+
+
